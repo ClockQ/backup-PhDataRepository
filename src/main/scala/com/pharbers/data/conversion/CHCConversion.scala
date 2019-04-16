c2cd5b93bab54d171f1053a4be2d9d41a0f7df52
clock <2567729201@qq.com>
2019-04-16 19:59:18 +0800

    正在重写CHC，我的分支更新要慎重

--- src/main/scala/com/pharbers/data/conversion/CHCConversion.scala
+++ src/main/scala/com/pharbers/data/conversion/CHCConversion.scala
@@ -1,13 +1,84 @@
 package com.pharbers.data.conversion
 
+import com.pharbers.data.model.chcData
 import org.apache.spark.sql.DataFrame
+import org.bson.types.ObjectId
 
 case class CHCConversion() extends PhDataConversion {
 
-	import com.pharbers.data.util.sparkDriver.ss.implicits._
 	import org.apache.spark.sql.functions._
+	import com.pharbers.data.util.sparkDriver.ss.implicits._
 
-	override def toERD(args: Map[String, DataFrame]): Map[String, DataFrame] = ???
+	override def toERD(args: Map[String, DataFrame]): Map[String, DataFrame] = {
+		val chcDF = args.getOrElse("chcDF", throw new Exception("not found chcDF"))
+
+		def getObjectId(): String = {
+			ObjectId.get().toString
+		}
+
+		val rddTemp = chcDF.na.fill("").distinct().toJavaRDD.rdd.map(x => chcData(x(0).toString, x(1).toString,
+			x(2).toString, x(3).toString, x(4).toString, x(5).toString, x(6).toString, x(7).toString, x(8).toString,
+			x(9).toString, x(10).toString, x(11).toString))
+
+		val midRDD = rddTemp.distinct().map { x =>
+			val chc_id = getObjectId()
+			x.chc_id = chc_id
+			x
+		}.groupBy(x => x.date).flatMap(x => {
+			val date_id = getObjectId()
+			x._2.map(y => {
+				y.date_id = date_id
+				y
+			})
+		}).groupBy(x => x.city).flatMap(x => {
+			val city_id = getObjectId()
+			x._2.map(y => {
+				y.city_id = city_id
+				y
+			})
+		}).groupBy(x => x.sales + x.units).flatMap(x => {
+			val revenue_id = getObjectId()
+			x._2.map(y => {
+				y.revenue_id = revenue_id
+				y
+			})
+		}).groupBy(x => x.prodName + x.packId + x.pack + x.moleName + x.moleNameCN + x.manufacture).flatMap(x => {
+			val product_id = getObjectId()
+			x._2.map(y => {
+				y.product_id = product_id
+				y
+			})
+		}).groupBy(x => x.packId + x.pack).flatMap(x => {
+			val pack_id = getObjectId()
+			x._2.map(y => {
+				y.pack_id = pack_id
+				y
+			})
+		}).groupBy(x => x.atc3 + x.oadType).flatMap(x => {
+			val oad_id = getObjectId()
+			x._2.map(y => {
+				y.oad_id = oad_id
+				y
+			})
+		}).groupBy(x => x.moleName + x.moleNameCN).flatMap(x => {
+			val mole_id = getObjectId()
+			x._2.map(y => {
+				y.mole_id = mole_id
+				y
+			})
+		}).groupBy(x => x.manufacture).flatMap(x => {
+			val manufacture_id = getObjectId()
+			x._2.map(y => {
+				y.manufacture_id = manufacture_id
+				y
+			})
+		}).cache()
+
+
+//		midRDD.toDF("_id", "data", "period").show(false)
+		println(midRDD.count())
+		Map()
+	}
 
 	override def toDIS(args: Map[String, DataFrame]): Map[String, DataFrame] = {
 		val chcBaseDF = args.getOrElse("chcBaseDF", throw new Exception("not found hospBaseDF"))
